ab9d5e7b8fc1574ad8440a18ed98c03be92bf484
Niina Varvemaa <p31247@edu.sasky.fi>
2025-11-23 17:40:14 +0200

    config-määrityksille oma tiedosto

--- public/index.php
+++ public/index.php
@@ -1,19 +1,37 @@
 <?php
 
+// Suoritetaan projektin alustusskripti.
+require_once '../src/init.php';
+
+
   // Siistitään polku urlin alusta ja mahdolliset parametrit urlin lopusta.
-  // Siistimisen jälkeen osoite /~koodaaja/lanify/tapahtuma?id=1 on 
+  // Siistimisen jälkeen osoite /~koodaaja/naytto/paja?id=1 on 
   // lyhentynyt muotoon /tapahtuma.
-  $request = str_replace('/~p31247/naytto','',$_SERVER['REQUEST_URI']);
+ $request = str_replace($config['urls']['baseUrl'],'',$_SERVER['REQUEST_URI']);
+
+
   $request = strtok($request, '?');
 
+
+
   // Selvitetään mitä sivua on kutsuttu ja suoritetaan sivua vastaava 
+
   // käsittelijä.
+
   if ($request === '/' || $request === '/pajat') {
-    echo '<h1>Kaikki joulupajat</h1>';
+
+    echo '<h1>Kaikki pajat</h1>';
+
   } else if ($request === '/paja') {
+
     echo '<h1>Yksittäisen pajan tiedot</h1>';
+
   } else {
+
     echo '<h1>Pyydettyä sivua ei löytynyt :(</h1>';
+
   }
+
+
 
 ?> 

91b525980d154455506be51e5f8f685ecdab7f67
Niina Varvemaa <p31247@edu.sasky.fi>
2025-11-24 21:54:02 +0200

    kirjautumistietojen tulostus

--- src/view/template.php
+++ src/view/template.php
@@ -6,9 +6,20 @@
     <meta charset="UTF-8">    
   </head>
   <body>
-       <header>
-      <h1><a href="<?=BASEURL?>">Tonttulan Joulupajat</a></h1>
+          <header>
+      <h1><a href="<?=BASEURL?>">Tonttujan joulupajat</a></h1>
+      <div class="profile">
+        <?php
+          if (isset($_SESSION['user'])) {
+            echo "<div>$_SESSION[user]</div>";
+            echo "<div><a href='logout'>Kirjaudu ulos</a></div>";
+          } else {
+            echo "<div><a href='kirjaudu'>Kirjaudu</a></div>";
+          }
+        ?>
+      </div>
     </header>
+
 
     <section>
       <?=$this->section('content')?>
